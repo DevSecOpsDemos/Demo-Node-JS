9d42195abbe4a63cd30f690695b93d137767c862
AmanShrivastava10 <shri.aman999.1474@gmail.com>
2021-05-25 09:51:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello DevOps SecOps Demo!");
+    response.end("Hello DevOps SecOps Demo! CI-CD Pipeline is now ready");
 
 });
 
